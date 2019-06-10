23bcaa70b4381d530bc344015f23c40e8452a799
lowlyHero <e.nunn1009@gmail.com>
2019-06-10 16:00:23 -0400

    Starting structure of login form and container. imported more dependencies

---
+++ tipsease/src/Components/Form.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Form = () => {
+    <div>
+        <form>
+            <h1>Login</h1>
+            <input type='text' />
+            <input type='password' />
+            <button>Login</button>
+        </form>
+    </div>
+}
+
+export default Form
