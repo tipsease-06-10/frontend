a4c885bf9de2dd87b6baf3f531c4af155cc83ee2
lowlyHero <e.nunn1009@gmail.com>
2019-06-12 10:51:23 -0400

    starting login actions and auth token process

---
+++ tipsease/src/Components/Login.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Login = ({
+    username: 'admin',
+    password: 'password'
+});
+
+export default Login;
--- tipsease/src/actions/index.js
+++ tipsease/src/actions/index.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-export const FETCH_WORKER = 'FETCH_CHARACTER';
+//============================ FETCH_WORKER_DATA ACTIONS
+
+export const FETCH_WORKER = 'FETCH_WORKER';
 export const FETCH_SUCCESS = 'FETCH_SUCCESS';
 export const FETCH_FAIL = 'FETCH_FAIL'
 
@@ -23,3 +25,30 @@ export const getWorkers = () => dispatch => {
         });
     });
 };
+
+
+//============================ LOGIN ACTIONS 
+
+export const LOGIN_START = 'LOGIN_START';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAIL = 'LOGIN_FAIL';
+
+export const login = res => dispatch => {
+    dispatch({ LOGIN_START });
+    axios
+    .post(
+        'https://eztip.herokuapp.com/login/'
+    )
+    .then(
+        console.log(res)
+    )
+    .catch(err => {
+            dispatch({
+                type: LOGIN_FAIL,
+                payload: err
+            });
+        });
+    };
+
+
+    //========================
