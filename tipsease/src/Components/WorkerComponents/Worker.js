669a7c82087fb9708c4cf0f6fb88de70204549d6
lowlyHero <e.nunn1009@gmail.com>
2019-06-14 17:02:44 -0400

    Committing latest work from debugging in demo lol

--- tipsease/src/Components/WorkerComponents/Worker.js
+++ tipsease/src/Components/WorkerComponents/Worker.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 const Worker = props => {
     return (
+    <div className='worker-container container'>
         <ul>
          <li><strong>First Name: </strong>{props.worker.first_name}</li>
          <li><strong>Last Name: </strong>{props.worker.last_name}</li>
@@ -12,6 +13,7 @@ const Worker = props => {
          <li><strong>Username: </strong>{props.worker.username}</li>
          <li><strong>Working Since: </strong>{props.worker.working_since}</li>
         </ul>
+    </div>
     )
     
 }
